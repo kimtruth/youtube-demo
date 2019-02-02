2930ec2ded3018f5677b43a6fd80ae569351b1db
Youngho Kim <miku@korea.ac.kr>
2019-02-02 17:11:09 +0900

    더보기를 누르면 흰 메뉴가 올라오게함

--- youtube demo/Sources/ViewControllers/SettingLauncher.swift
+++ youtube demo/Sources/ViewControllers/SettingLauncher.swift
@@ -12,8 +12,11 @@ final class SettingLauncher {
   
   // MARK: Constants
   
+  private struct Metric {
+    static let collectionViewHeight = 300.f
+  }
   private struct Constant {
-    static let animationDuration = 0.3
+    static let animationDuration = 0.2
   }
   
   // MARK: UI
@@ -22,20 +25,36 @@ final class SettingLauncher {
     $0.alpha = 0
     $0.backgroundColor = UIColor(white: 0, alpha: 0.5)
   }
+  private let collectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: UICollectionViewFlowLayout()
+  ).then {
+      $0.backgroundColor = .white
+  }
   
   func showSettings() {
-    if let window = UIApplication.shared.keyWindow {
-      window.addSubview(self.blackView)
-      
-      self.blackView.frame = window.frame
-      
-      let recognizer = UITapGestureRecognizer(target: self, action: #selector(self.blackViewDidTap))
-      self.blackView.addGestureRecognizer(recognizer)
-      
-      UIView.animate(withDuration: Constant.animationDuration, delay: 0, options: .curveEaseIn, animations: {
-        self.blackView.alpha = 1
-      })
-    }
+    guard let window = UIApplication.shared.keyWindow else { return }
+    window.addSubview(self.blackView)
+    window.addSubview(self.collectionView)
+    
+    self.blackView.frame = window.frame
+    
+    let recognizer = UITapGestureRecognizer(target: self, action: #selector(self.blackViewDidTap))
+    self.blackView.addGestureRecognizer(recognizer)
+    
+    self.collectionView.frame = .init(
+      x: 0,
+      y: window.frame.height,
+      width: window.frame.width,
+      height: Metric.collectionViewHeight
+    )
+    
+    UIView.animate(withDuration: Constant.animationDuration, delay: 0, options: .curveEaseIn, animations: {
+      self.blackView.alpha = 1
+      var frame = self.collectionView.frame
+      frame.origin.y -= self.collectionView.frame.height
+      self.collectionView.frame = frame
+    })
   }
   
   // MARK: Actions
@@ -43,8 +62,12 @@ final class SettingLauncher {
   @objc private func blackViewDidTap() {
     UIView.animate(withDuration: Constant.animationDuration, delay: 0, options: .curveEaseIn, animations: {
       self.blackView.alpha = 0
+      var frame = self.collectionView.frame
+      frame.origin.y += self.collectionView.frame.height
+      self.collectionView.frame = frame
     }) { finished in
       self.blackView.removeFromSuperview()
+      self.collectionView.removeFromSuperview()
     }
   }
 }
