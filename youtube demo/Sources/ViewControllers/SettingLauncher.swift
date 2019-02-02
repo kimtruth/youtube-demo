21248bec377c7a6a208874feb74e57cb9fee243a
Youngho Kim <miku@korea.ac.kr>
2019-02-02 16:54:05 +0900

    더보기를 누를경우 반투명한 검은색 배경이 되게함

--- youtube demo/Sources/ViewControllers/SettingLauncher.swift
+++ youtube demo/Sources/ViewControllers/SettingLauncher.swift
@@ -10,7 +10,41 @@ import UIKit
 
 final class SettingLauncher {
   
+  // MARK: Constants
+  
+  private struct Constant {
+    static let animationDuration = 0.3
+  }
+  
+  // MARK: UI
+  
+  private let blackView = UIView().then {
+    $0.alpha = 0
+    $0.backgroundColor = UIColor(white: 0, alpha: 0.5)
+  }
+  
   func showSettings() {
-    print("show Settings")
+    if let window = UIApplication.shared.keyWindow {
+      window.addSubview(self.blackView)
+      
+      self.blackView.frame = window.frame
+      
+      let recognizer = UITapGestureRecognizer(target: self, action: #selector(self.blackViewDidTap))
+      self.blackView.addGestureRecognizer(recognizer)
+      
+      UIView.animate(withDuration: Constant.animationDuration, delay: 0, options: .curveEaseIn, animations: {
+        self.blackView.alpha = 1
+      })
+    }
+  }
+  
+  // MARK: Actions
+  
+  @objc private func blackViewDidTap() {
+    UIView.animate(withDuration: Constant.animationDuration, delay: 0, options: .curveEaseIn, animations: {
+      self.blackView.alpha = 0
+    }) { finished in
+      self.blackView.removeFromSuperview()
+    }
   }
 }
