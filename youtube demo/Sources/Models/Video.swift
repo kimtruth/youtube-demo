e1efb97464e27844b1bfc07b87f17bc1c2dd3021
Youngho Kim <miku@korea.ac.kr>
2019-02-02 15:06:21 +0900

    Video 모델 작성

---
+++ youtube demo/Sources/Models/Video.swift
@@ -0,0 +1,15 @@
+//
+//  Video.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 2..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+struct Video {
+  var thumbnailImageName: String
+  var title: String
+  var numberOfViews: Int
+}
