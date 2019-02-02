b0aab5b03fdd5d9bd1d30d4af9b8488c041982fe
Youngho Kim <miku@korea.ac.kr>
2019-02-02 14:15:21 +0900

    MenuBar 클래스 추가

---
+++ youtube demo/Sources/Views/MenuBar.swift
@@ -0,0 +1,20 @@
+//
+//  MenuBar.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 2..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+final class MenuBar: UIView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
