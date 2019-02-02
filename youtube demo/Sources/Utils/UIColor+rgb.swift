77adc77e38691fb5f0c451fff56216031721a75c
Youngho Kim <miku@korea.ac.kr>
2019-02-02 13:32:20 +0900

    UIColor+rgb 작성

---
+++ youtube demo/Sources/Utils/UIColor+rgb.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+rgb.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 2..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    return UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
+  }
+}
