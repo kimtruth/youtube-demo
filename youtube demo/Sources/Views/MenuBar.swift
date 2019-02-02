18a6c17f76a41806364fa93252a2e471a87ebbd5
Youngho Kim <miku@korea.ac.kr>
2019-02-02 14:25:51 +0900

    MenuBar에 UICollectionView 추가

--- youtube demo/Sources/Views/MenuBar.swift
+++ youtube demo/Sources/Views/MenuBar.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 final class MenuBar: UIView {
+  
+  // MARK: Properties
+  
+  private let cellId = "MenuCell"
+  
+  // MARK: UI
+  
+  private lazy var collectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: UICollectionViewFlowLayout()
+  ).then {
+    $0.backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31)
+    $0.dataSource = self
+    $0.delegate = self
+  }
+  
+  // MARK: View Life Cycles
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
+    
+    self.addSubview(self.collectionView)
+    self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: self.cellId)
+    self.collectionView.snp.makeConstraints { (make) in
+      make.edges.equalToSuperview()
+    }
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension MenuBar: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 4
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellId, for: indexPath)
+    cell.backgroundColor = .white
+    
+    return cell
+  }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension MenuBar: UICollectionViewDelegateFlowLayout {
   
 }
