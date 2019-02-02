87ed9c66767522ebaadb1e1323c7b494b640074f
Youngho Kim <miku@korea.ac.kr>
2019-02-02 15:14:32 +0900

    Channel 모델 작성

---
+++ youtube demo/Sources/Models/Channel.swift
@@ -0,0 +1,14 @@
+//
+//  Channel.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 2..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+struct Channel {
+  var name: String
+  var profileImageName: String
+}
