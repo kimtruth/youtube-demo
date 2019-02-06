bc33d0debefe2483f30158aad5293e66da5269aa
Youngho Kim <miku@korea.ac.kr>
2019-02-06 20:06:58 +0900

    Setting 모델 작성

---
+++ youtube demo/Sources/Models/Setting.swift
@@ -0,0 +1,14 @@
+//
+//  Setting.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 6..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+struct Setting {
+  let name: String
+  let imageName: String
+}
