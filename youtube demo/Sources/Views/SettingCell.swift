52e66e66b4211d1b9c08367883f5b55acfe53c47
Youngho Kim <miku@korea.ac.kr>
2019-02-06 19:53:33 +0900

    SettingCell 클래스 추가

---
+++ youtube demo/Sources/Views/SettingCell.swift
@@ -0,0 +1,19 @@
+//
+//  SettingCell.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 6..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+final class SettingCell: BaseCell {
+  
+  // MARK: Setups
+  
+  override func setupViews() {
+    super.setupViews()
+    
+  }
+}
