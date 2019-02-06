2aeca1da0033dae70ebf667530fea3d238c469fb
Youngho Kim <miku@korea.ac.kr>
2019-02-06 20:06:50 +0900

    SettingCell 의 UI 추가

--- youtube demo/Sources/Views/SettingCell.swift
+++ youtube demo/Sources/Views/SettingCell.swift
@@ -10,11 +10,32 @@ import UIKit
 
 final class SettingCell: BaseCell {
   
+  // MARK: UI
+  
+  private let nameLabel = UILabel().then {
+    $0.text = "Setting"
+  }
+  private let iconImageView = UIImageView().then {
+    $0.image = UIImage(named: "settings")
+  }
+  
   // MARK: Setups
   
   override func setupViews() {
     super.setupViews()
     
-    self.backgroundColor = .blue
+    self.backgroundColor = .lightGray
+    self.addSubview(self.nameLabel)
+    self.addSubview(self.iconImageView)
+    
+    
+    self.nameLabel.snp.makeConstraints { (make) in
+      make.left.equalTo(self.iconImageView.snp.right)
+      make.top.right.bottom.equalToSuperview()
+    }
+    self.iconImageView.snp.makeConstraints { (make) in
+      make.top.left.bottom.equalToSuperview()
+      make.width.equalTo(self.iconImageView.snp.height)
+    }
   }
 }
