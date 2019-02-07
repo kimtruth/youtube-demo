7974833e2e6226a15aeb2808978daa6bbe3c420d
Youngho Kim <miku@korea.ac.kr>
2019-02-07 15:53:16 +0900

    Feed 타입 추가

---
+++ youtube demo/Sources/Types/Feed.swift
@@ -0,0 +1,15 @@
+//
+//  Feed.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 7..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import Foundation
+
+enum Feed {
+  case home
+  case trending
+  case subscriptions
+}
