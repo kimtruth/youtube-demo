1fd8bf71398c242172fe860c31625cac482f96db
Youngho Kim <miku@korea.ac.kr>
2019-02-09 19:59:37 +0900

    VideoPlayerView 클래스 추가

---
+++ youtube demo/Sources/Views/VideoPlayerView.swift
@@ -0,0 +1,22 @@
+//
+//  VideoPlayerView.swift
+//  youtube demo
+//
+//  Created by truth on 2019. 2. 9..
+//  Copyright © 2019년 kimtruth. All rights reserved.
+//
+
+import UIKit
+
+final class VideoPlayerView: UIView {
+  
+  // MARK: Initializing
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
